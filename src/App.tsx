367654eceff14cb46cdc8878b4e98fa2a9628ccb
Yevhenii Kyrychenko <kirichevg@gmail.com>
2026-04-08 00:03:20 +0300

    feat(ui): added header

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { WeatherSearch } from './features/weather/WeatherSearch';
 import { WeatherDisplay } from './features/weather/WeatherDisplay';
 import { HistoryList } from './features/history/HistoryList';
 import { UndoToast } from './components/UndoToast';
+import { Header } from './components/Header';
 
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-slate-50 flex flex-col items-center justify-start pt-20 px-4 gap-6">
+      <Header />
+      <div className="min-h-screen bg-slate-50 flex flex-col items-center justify-start pt-10 px-4 gap-6">
         <WeatherSearch />
         <WeatherDisplay />
         <HistoryList />
